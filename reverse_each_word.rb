a87282978b5fed34b9206ac6aeb11cadc48cf98d
Elliot Blanchard <public@elliotblanchard.com>
2020-04-11 18:57:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,9 @@
 def reverse_each_word(sentence)
   sentence_array = sentence.split(" ")
   response = ""
-  sentence_array.each {|word| response << word.reverse}
+  sentence_array.each do |word|
+    response << word.reverse
+    response << " "
+  end
   response
 end
